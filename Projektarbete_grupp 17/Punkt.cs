f70b76c7e1a171f2c98d917625ea3590ca154249
Simon Thor <simon.thor.9203@student.uu.se>
2021-11-19 15:51:39 +0100

    Punktklass

---
+++ Projektarbete_grupp 17/Punkt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektarbete_grupp_17
+{
+    class Punkt
+    {
+        int y;
+        int x;
+        int pointscore;
+
+        public Punkt (string inputX, string inputY, string _pointscore)
+        {
+            y = int.Parse(inputY);
+            x = int.Parse(inputX);
+            pointscore = int.Parse(_pointscore);
+        }
+    }
+}
