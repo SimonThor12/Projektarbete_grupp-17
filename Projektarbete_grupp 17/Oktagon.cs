e4cb3d3b3924c793d880c61baac1046b4a6f5c90
Simon Thor <simon.thor.9203@student.uu.se>
2021-11-19 16:26:20 +0100

    Oktagon

--- Projektarbete_grupp 17/Oktagon.cs
+++ Projektarbete_grupp 17/Oktagon.cs
@@ -5,8 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Projektarbete_grupp_17
-{ 
-    class Oktagon: Former
+{
+    class Oktagon : Former
     {
+        public int sidAntal { get; private set; }
+        public int sidLängd { get; private set; }
+
+        public Oktagon ()
+        {
+
+        }
+        double GetArea()
+        {
+            int area = 1/4*sidAntal*Math.Pow(sidLängd,2) + 
+        }
+            
+        double GetPerimeter()
+        {
+            int perimeter = sidAntal * sidLängd;
+            return perimeter;
+        } 
+            
     }
 }
