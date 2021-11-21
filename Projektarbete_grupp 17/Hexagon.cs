a82f2991460bde4e2e81a1cc4af5a026ba175ab6
shayan2K <shayanbahmani00@gmail.com>
2021-11-21 20:31:27 +0100

    Uträkningar för pengaton och hexagon. /Shayan

--- Projektarbete_grupp 17/Hexagon.cs
+++ Projektarbete_grupp 17/Hexagon.cs
@@ -8,5 +8,16 @@ namespace Projektarbete_grupp_17
 {
     class Hexagon: Former
     {
+        public int lengthOfSides { get; private set; }
+        double GetPerimeter()
+        {
+            int perimeter = 6 * lengthOfSides;
+            return perimeter;
+        }
+        double GetArea()
+        {
+            double area = 3 * Math.Sqrt(3) / 2 * Math.Pow(5, 2);
+            return area;
+        }
     }
 }
--- Projektarbete_grupp 17/Pentagon.cs
+++ Projektarbete_grupp 17/Pentagon.cs
@@ -8,5 +8,16 @@ namespace Projektarbete_grupp_17
 {
     class Pentagon: Former
     {
+        public int lengthOfSides { get; private set; }
+        double GetPerimeter()
+        {
+            int perimeter = 5 * lengthOfSides;
+            return perimeter;
+        }
+        double GetArea()
+        {
+            double area = 0.25 * Math.Sqrt(5 * (5 + 2 * Math.Sqrt(5))) * Math.Pow(lengthOfSides, 2);
+            return area;
+        }
     }
 }
