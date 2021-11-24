8767be78c3b6de23092eb6cd58dc95e576dcb112
SimonThor12 <92580924+SimonThor12@users.noreply.github.com>
2021-11-24 12:46:57 +0100

    handleinput start

---
+++ Projektarbete_grupp 17/HandleInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektarbete_grupp_17
+{
+    class HandleInput
+    {
+        public HandleInput (string Input)
+        {
+
+        }
+        public string punkt = "1,1,5;2,2,10";
+
+        string figur = "Circle,5,3,100";
+
+        public string[] arrKommand = punkt.Split(";");
+
+        string punkt1 = arrKommand[0];
+        string punkt2 = arrKommand[1];
+
+        string[] punktArray = punkt1.Split(",");
+        string Xvärde = punktArray[0];
+        string Yvärde = punktArray[1];
+        string Score = punktArray[2];
+    }
+}
--- Projektarbete_grupp 17/Program.cs
+++ Projektarbete_grupp 17/Program.cs
@@ -6,20 +6,7 @@ namespace Projektarbete_grupp_17
     {
         static void Main(string[] args)
         {
-            IPair<int, int> koordinater = new Punkt<int, int>();
-            string kommando = "1,1,5;2,2,10";
-
-            string figur = "Circle,5,3,100";
-
-            string[] arrKommand = kommando.Split(";");
-
-            string nykommand1 = arrKommand[0];
-            string nykommand2 = arrKommand[1];
-
-            string[] arrNyKommand = nykommand1.Split(",");
-            string _ny1Kommand1 = arrNyKommand[0];
-            string _ny2Kommand2 = arrNyKommand[1];
-            string _ny3Kommand3 = arrNyKommand[2];
+          
         }
     }
 }
