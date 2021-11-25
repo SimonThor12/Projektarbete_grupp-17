d73430c12ea4b5695ed01e23e118c5e56f040f4d
Simon Thor <simon.thor.9203@student.uu.se>
2021-11-25 13:56:06 +0100

    handleoutput

---
+++ Projektarbete_grupp 17/HandleOutput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektarbete_grupp_17
+{
+    class HandleOutput
+    {
+    }
+}
