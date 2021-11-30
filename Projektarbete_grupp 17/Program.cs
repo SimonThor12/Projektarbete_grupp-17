61b1508cc324e333baa84ef29597c0214b46b4e6
Martin Starck <martin.starck.7728@student.uu.se>
2021-11-30 15:30:28 +0100

    cwwq

--- Projektarbete_grupp 17/Program.cs
+++ Projektarbete_grupp 17/Program.cs
@@ -6,14 +6,17 @@ namespace Projektarbete_grupp_17
     {
         static void Main(string[] args)
         {
+            
 
-            //HandleInput1 punkter = new HandleInput1("1,2;3,5,7");
+            HandleInput1 punkter = new HandleInput1("1,2;3,5,7;5");
+            Koordinat kordinat = new Koordinat(3, 6);
 
-            //HandleInput2 Former = new HandleInput2("CIRCLE,2,3,100");
+            HandleInput2 Former = new HandleInput2("CIRCLE,2,3,100");
             HandleInput3 Shapescore = new HandleInput3("CIRCLE,4");
-
-            //HandleInput2 fyrkaten = new HandleInput2("Square, 3, 5, 20");
-            //HandleOutput fyrnakten = new HandleOutput(punkter, fyrkaten, Shapescore);
+            Cirkel cirkel = new Cirkel(Former);
+            Console.WriteLine(cirkel.IsInside(kordinat));
+            HandleInput2 fyrkaten = new HandleInput2("Square, 3, 5, 20");
+            HandleOutput fyrnakten = new HandleOutput(punkter, Former, Shapescore);
 
         }
     }
--- Projektarbete_grupp 17/Punkt.cs
+++ Projektarbete_grupp 17/Punkt.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace Projektarbete_grupp_17
 {
-    class Punkt: Koordinat
+    /*class Punkt: Koordinat
     {
         
-    }
+    }*/
 }
