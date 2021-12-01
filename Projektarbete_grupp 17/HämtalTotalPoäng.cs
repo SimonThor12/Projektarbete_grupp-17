6105d37cbb9cc0f25484ffda94ca16919b8a3281
Martin Starck <martin.starck.7728@student.uu.se>
2021-12-01 10:02:42 +0100

    mumjokn

---
+++ Projektarbete_grupp 17/HämtalTotalPoäng.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektarbete_grupp_17
+{
+    class HämtalTotalPoäng
+    {
+        public long OutPut1Cirkel(Cirkel input, Koordinat punkten)
+        {
+            if (input.IsInside(punkten))
+            {
+                input.GetArea() * punkten.pointScore * 
+            }
+        }
+    }
+}
