b6c8ce507ea1ef33eda8f539ad93dec12464955a
Simon Thor <simon.thor.9203@student.uu.se>
2022-01-24 18:25:06 +0100

    asd

--- Projektarbete_grupp 17/Program.cs
+++ Projektarbete_grupp 17/Program.cs
@@ -8,9 +8,9 @@ namespace Projektarbete_grupp_17
         {
             
 
-            HandleInput3 ShapescoreInput = new HandleInput3("SQUARE,5;CIRCLE,10");
-            HandleInput2 FormerInput = new HandleInput2("SQUARE,5,3,10; CIRCLE, 1,1, 12", ShapescoreInput);
-            HandleInput1 Input1 = new HandleInput1("1,1,5");
+            HandleInput3 ShapescoreInput = new HandleInput3("TRIANGLE, 3;SQUARE,5;CIRCLE,10");
+            HandleInput2 FormerInput = new HandleInput2("TRIANGLE,0,0,60;SQUARE,5,3,10; CIRCLE, 1,1, 12", ShapescoreInput);
+            HandleInput1 Input1 = new HandleInput1("1,1,5;2,2,10");
             HandleAllInput input = new HandleAllInput(Input1, FormerInput, ShapescoreInput);
             HämtalTotalPoäng TotalPoäng = new HämtalTotalPoäng(input);
             Console.WriteLine(TotalPoäng.KalkTotalPoäng());
