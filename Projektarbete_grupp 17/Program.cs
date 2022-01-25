a014aefe030925a6b41ee048ac39f828de66e392
Martin Starck <martin.starck.7728@student.uu.se>
2022-01-25 14:36:05 +0100

    faf

--- Projektarbete_grupp 17/Program.cs
+++ Projektarbete_grupp 17/Program.cs
@@ -8,8 +8,8 @@ namespace Projektarbete_grupp_17
         {
             
 
-            HandleInput3 ShapescoreInput = new HandleInput3("TRIANGLE,3;SQUARE,5;CIRCLE,10");
-            HandleInput2 FormerInput = new HandleInput2("TRIANGLE,0,0,60; CIRCLE, 1,1, 12;SQUARE,5,3,10", ShapescoreInput);
+            HandleInput3 ShapescoreInput = new HandleInput3("TRIANGLE,3;SQUARE,5;CIRCLE,10; Octagon,7");
+            HandleInput2 FormerInput = new HandleInput2("TRIANGLE,0,0,60; CIRCLE, 1,1, 12;SQUARE,5,3,10; OCTAGON, 1,-20, 15", ShapescoreInput);
             HandleInput1 Input1 = new HandleInput1("1,1,5;2,2,10");
             HandleAllInput input = new HandleAllInput(Input1, FormerInput, ShapescoreInput);
             HämtalTotalPoäng TotalPoäng = new HämtalTotalPoäng(input);
